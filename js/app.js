67c8bd5984fc1c2849869ec2ea6024faad2722e2
DSoutter <duncan.soutter@gmail.com>
2021-08-26 15:14:44 +0100

    working delete button

--- js/app.js
+++ js/app.js
@@ -3,41 +3,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
-  // const list = document.querySelector('ul')
-  // list.appendChild(handleFormSubmit);
 
-  const resetForm = document.querySelector('#new-item-form')
-  resetForm.addEventListener('submit', handleFormReset)
+  const resetForm = document.querySelector('#new-item-form');
+  resetForm.addEventListener('submit', handleFormReset);
   
+  const deleteReadingList = document.querySelector('#delete');
+  deleteReadingList.addEventListener('click', handleDeleteAll)
 })
 
 
 const handleFormSubmit = function(event) {
   event.preventDefault();
 
-  const resultParagraph = document.querySelector('#reading-list')
-  const newResultParagraph = document.createElement('li')
-  newResultParagraph.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`
+  // Previously used but no longer needed: const resultParagraph = document.querySelector('#reading-list')
+  const newResultParagraph = document.createElement('li');
+  newResultParagraph.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
  
-  // newResultParagraph.classList.add()
-  const list = document.querySelector('ul')
-  list.appendChild(newResultParagraph)
+  const list = document.querySelector('ul');
+  list.appendChild(newResultParagraph);
 }
 
 const handleFormReset = function(event) {
-  // event.preventDefault();
-
-  const resetForm = document.querySelector('#new-item-form')
-  resetForm.reset()
-
-  // resetForm.textContent.reset(event)
+  const resetForm = document.querySelector('#new-item-form');
+  resetForm.reset();
 }
 
-    // adding a new element to the list
+const handleDeleteAll = function() {
+  let deleteList = document.querySelector('#reading-list');
+  deleteList.textContent = '';
 
-    // const newListItem = document.createElement('li');
-    // newListItem.textContent = "Purple";
-    // newListItem.classList.add('purple');
+}
 
-    // const list = document.querySelector('ul');
-    // list.appendChild(newListItem);

c6bbda778c0856a0d867ab9c5669b854e8953f94
DSoutter <duncan.soutter@gmail.com>
2021-08-26 14:32:38 +0100

    working adding to list

--- js/app.js
+++ js/app.js
@@ -3,8 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
+  // const list = document.querySelector('ul')
+  // list.appendChild(handleFormSubmit);
 
-
+  const resetForm = document.querySelector('#new-item-form')
+  resetForm.addEventListener('submit', handleFormReset)
+  
 })
 
 
@@ -12,5 +16,28 @@ const handleFormSubmit = function(event) {
   event.preventDefault();
 
   const resultParagraph = document.querySelector('#reading-list')
-  resultParagraph.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`
+  const newResultParagraph = document.createElement('li')
+  newResultParagraph.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`
+ 
+  // newResultParagraph.classList.add()
+  const list = document.querySelector('ul')
+  list.appendChild(newResultParagraph)
 }
+
+const handleFormReset = function(event) {
+  // event.preventDefault();
+
+  const resetForm = document.querySelector('#new-item-form')
+  resetForm.reset()
+
+  // resetForm.textContent.reset(event)
+}
+
+    // adding a new element to the list
+
+    // const newListItem = document.createElement('li');
+    // newListItem.textContent = "Purple";
+    // newListItem.classList.add('purple');
+
+    // const list = document.querySelector('ul');
+    // list.appendChild(newListItem);
